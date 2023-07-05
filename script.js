24e4e3135e869e18b5915dedff90b7c5a4b2e5e0
RubyCooper <119014321+RubyCooper@users.noreply.github.com>
2023-07-05 14:24:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,422 @@
+var photo01 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(1) a img");
+var photo02 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(2) a img");
+var photo03 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(3) a img");
+var photo04 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(4) a img");
+var photo05 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(5) a img");
+var photo06 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(6) a img");
+var photo07 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(7) a img");
+var photo08 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(8) a img");
+var photo09 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(9) a img");
+var photo10 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(10) a img");
+var photo11 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(11) a img");
+var photo12 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(12) a img");
+var photo13 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(13) a img");
+var photo14 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(14) a img");
+var photo15 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(15) a img");
+var photo16 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(16) a img");
+var photo17 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(17) a img");
+var photo18 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(18) a img");
+var photo19 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(19) a img");
+var photo20 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(20) a img");
+var photo21 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(21) a img");
+var photo22 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(22) a img");
+var photo23 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(23) a img");
+var photo24 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(24) a img");
+var photo25 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(25) a img");
+var photo26 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(26) a img");
+var photo27 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(27) a img");
+var photo28 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(28) a img");
+var photo29 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(29) a img");
+var photo30 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(30) a img");
+var photo31 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(31) a img");
+var photo32 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(32) a img");
+var photo33 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(33) a img");
+var photo34 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(34) a img");
+var photo35 = document.querySelector("#photo-page main section ul:nth-of-type(1) li:nth-of-type(35) a img");
+
+
+
+var thumbPhoto01 = document.querySelector("#gallery-page main ul li:nth-of-type(1) a");
+var thumbPhoto02 = document.querySelector("#gallery-page main ul li:nth-of-type(2) a");
+var thumbPhoto03 = document.querySelector("#gallery-page main ul li:nth-of-type(3) a");
+var thumbPhoto04 = document.querySelector("#gallery-page main ul li:nth-of-type(4) a");
+var thumbPhoto05 = document.querySelector("#gallery-page main ul li:nth-of-type(5) a");
+var thumbPhoto06 = document.querySelector("#gallery-page main ul li:nth-of-type(6) a");
+var thumbPhoto07 = document.querySelector("#gallery-page main ul li:nth-of-type(7) a");
+var thumbPhoto08 = document.querySelector("#gallery-page main ul li:nth-of-type(8) a");
+var thumbPhoto09 = document.querySelector("#gallery-page main ul li:nth-of-type(9) a");
+var thumbPhoto10 = document.querySelector("#gallery-page main ul li:nth-of-type(10) a");
+var thumbPhoto11 = document.querySelector("#gallery-page main ul li:nth-of-type(11) a");
+var thumbPhoto12 = document.querySelector("#gallery-page main ul li:nth-of-type(12) a");
+var thumbPhoto13 = document.querySelector("#gallery-page main ul li:nth-of-type(13) a");
+var thumbPhoto14 = document.querySelector("#gallery-page main ul li:nth-of-type(14) a");
+var thumbPhoto15 = document.querySelector("#gallery-page main ul li:nth-of-type(15) a");
+var thumbPhoto16 = document.querySelector("#gallery-page main ul li:nth-of-type(16) a");
+var thumbPhoto17 = document.querySelector("#gallery-page main ul li:nth-of-type(17) a");
+var thumbPhoto18 = document.querySelector("#gallery-page main ul li:nth-of-type(18) a");
+var thumbPhoto19 = document.querySelector("#gallery-page main ul li:nth-of-type(19) a");
+var thumbPhoto20 = document.querySelector("#gallery-page main ul li:nth-of-type(20) a");
+var thumbPhoto21 = document.querySelector("#gallery-page main ul li:nth-of-type(21) a");
+var thumbPhoto22 = document.querySelector("#gallery-page main ul li:nth-of-type(22) a");
+var thumbPhoto23 = document.querySelector("#gallery-page main ul li:nth-of-type(23) a");
+var thumbPhoto24 = document.querySelector("#gallery-page main ul li:nth-of-type(24) a");
+var thumbPhoto25 = document.querySelector("#gallery-page main ul li:nth-of-type(25) a");
+var thumbPhoto26 = document.querySelector("#gallery-page main ul li:nth-of-type(26) a");
+var thumbPhoto27 = document.querySelector("#gallery-page main ul li:nth-of-type(27) a");
+var thumbPhoto28 = document.querySelector("#gallery-page main ul li:nth-of-type(28) a");
+var thumbPhoto29 = document.querySelector("#gallery-page main ul li:nth-of-type(29) a");
+var thumbPhoto30 = document.querySelector("#gallery-page main ul li:nth-of-type(30) a");
+var thumbPhoto31 = document.querySelector("#gallery-page main ul li:nth-of-type(31) a");
+var thumbPhoto32 = document.querySelector("#gallery-page main ul li:nth-of-type(32) a");
+var thumbPhoto33 = document.querySelector("#gallery-page main ul li:nth-of-type(33) a");
+var thumbPhoto34 = document.querySelector("#gallery-page main ul li:nth-of-type(34) a");
+var thumbPhoto35 = document.querySelector("#gallery-page main ul li:nth-of-type(35) a");
+
+var homeThumbPhoto01 = document.querySelector("#home-page main ul li:nth-of-type(1) a");
+var homeThumbPhoto02 = document.querySelector("#home-page main ul li:nth-of-type(2) a");
+var homeThumbPhoto03 = document.querySelector("#home-page main ul li:nth-of-type(3) a");
+var homeThumbPhoto04 = document.querySelector("#home-page main ul li:nth-of-type(4) a");
+var homeThumbPhoto05 = document.querySelector("#home-page main ul li:nth-of-type(5) a");
+var homeThumbPhoto06 = document.querySelector("#home-page main ul li:nth-of-type(6) a");
+var homeThumbPhoto07 = document.querySelector("#home-page main ul li:nth-of-type(7) a");
+var homeThumbPhoto08 = document.querySelector("#home-page main ul li:nth-of-type(8) a");
+var homeThumbPhoto09 = document.querySelector("#home-page main ul li:nth-of-type(9) a");
+var homeThumbPhoto10 = document.querySelector("#home-page main ul li:nth-of-type(10) a");
+var homeThumbPhoto11 = document.querySelector("#home-page main ul li:nth-of-type(11) a");
+var homeThumbPhoto12 = document.querySelector("#home-page main ul li:nth-of-type(12) a");
+var homeThumbPhoto13 = document.querySelector("#home-page main ul li:nth-of-type(13) a");
+var homeThumbPhoto14 = document.querySelector("#home-page main ul li:nth-of-type(14) a");
+var homeThumbPhoto15 = document.querySelector("#home-page main ul li:nth-of-type(15) a");
+var homeThumbPhoto16 = document.querySelector("#home-page main ul li:nth-of-type(16) a");
+var homeThumbPhoto17 = document.querySelector("#home-page main ul li:nth-of-type(17) a");
+var homeThumbPhoto18 = document.querySelector("#home-page main ul li:nth-of-type(18) a");
+var homeThumbPhoto19 = document.querySelector("#home-page main ul li:nth-of-type(19) a");
+var homeThumbPhoto20 = document.querySelector("#home-page main ul li:nth-of-type(20) a");
+
+
+var nextButton = document.querySelector('#photo-page main section ul:nth-of-type(2) li:nth-of-type(2) button');
+var backButton = document.querySelector('#photo-page main section ul:nth-of-type(2) li:nth-of-type(1) button');
+
+var allPhotos = document.querySelectorAll("#photo-page main section ul:nth-of-type(1) li");
+
+var clickedPhotoSave;
+var currentPhoto = clickedPhotoSave;
+var arrayPhotoPosition = 0;
+
+var body = document.querySelector("#photo-page");
+
+var toggleSwitch = document.querySelector("header button");
+
+// //////////////////
+// EventListeners: //
+// //////////////////
+
+
+if (document.body.id == "gallery-page") {
+
+    thumbPhoto01.addEventListener("click", () => {
+        photoSave(1);
+    });
+
+    thumbPhoto02.addEventListener("click", () => {
+        photoSave(2);
+    });
+
+    thumbPhoto03.addEventListener("click", () => {
+        photoSave(3);
+    });
+
+    thumbPhoto04.addEventListener("click", () => {
+        photoSave(4);
+    });
+
+    thumbPhoto05.addEventListener("click", () => {
+        photoSave(5);
+    });
+
+    thumbPhoto06.addEventListener("click", () => {
+        photoSave(6);
+    });
+
+    thumbPhoto07.addEventListener("click", () => {
+        photoSave(7);
+    });
+
+    thumbPhoto08.addEventListener("click", () => {
+        photoSave(8);
+    });
+
+    thumbPhoto09.addEventListener("click", () => {
+        photoSave(9);
+    });
+
+    thumbPhoto10.addEventListener("click", () => {
+        photoSave(10);
+    });
+
+    thumbPhoto11.addEventListener("click", () => {
+        photoSave(11);
+    });
+
+    thumbPhoto12.addEventListener("click", () => {
+        photoSave(12);
+    });
+
+    thumbPhoto13.addEventListener("click", () => {
+        photoSave(13);
+    });
+
+    thumbPhoto14.addEventListener("click", () => {
+        photoSave(14);
+    });
+
+    thumbPhoto15.addEventListener("click", () => {
+        photoSave(15);
+    });
+
+    thumbPhoto16.addEventListener("click", () => {
+        photoSave(16);
+    });
+
+    thumbPhoto17.addEventListener("click", () => {
+        photoSave(17);
+    });
+
+    thumbPhoto18.addEventListener("click", () => {
+        photoSave(18);
+    });
+
+    thumbPhoto19.addEventListener("click", () => {
+        photoSave(19);
+    });
+
+    thumbPhoto20.addEventListener("click", () => {
+        photoSave(20);
+    });
+
+    thumbPhoto21.addEventListener("click", () => {
+        photoSave(21);
+    });
+
+    thumbPhoto22.addEventListener("click", () => {
+        photoSave(22);
+    });
+
+    thumbPhoto23.addEventListener("click", () => {
+        photoSave(23);
+    });
+
+    thumbPhoto24.addEventListener("click", () => {
+        photoSave(24);
+    });
+
+    thumbPhoto25.addEventListener("click", () => {
+        photoSave(25);
+    });
+
+    thumbPhoto26.addEventListener("click", () => {
+        photoSave(26);
+    });
+
+    thumbPhoto27.addEventListener("click", () => {
+        photoSave(27);
+    });
+
+    thumbPhoto28.addEventListener("click", () => {
+        photoSave(28);
+    });
+
+    thumbPhoto29.addEventListener("click", () => {
+        photoSave(29);
+    });
+
+    thumbPhoto30.addEventListener("click", () => {
+        photoSave(30);
+    });
+
+    thumbPhoto31.addEventListener("click", () => {
+        photoSave(31);
+    });
+
+    thumbPhoto32.addEventListener("click", () => {
+        photoSave(32);
+    });
+
+    thumbPhoto33.addEventListener("click", () => {
+        photoSave(33);
+    });
+
+    thumbPhoto34.addEventListener("click", () => {
+        photoSave(34);
+    });
+
+    thumbPhoto25.addEventListener("click", () => {
+        photoSave(35);
+    });
+}
+
+
+if(document.body.id == "home-page") {
+    homeThumbPhoto01.addEventListener("click", () => {
+        photoSave(1);
+    });
+
+    homeThumbPhoto02.addEventListener("click", () => {
+        photoSave(19);
+    });
+
+    homeThumbPhoto03.addEventListener("click", () => {
+        photoSave(29);
+    });
+
+    homeThumbPhoto04.addEventListener("click", () => {
+        photoSave(25);
+    });
+
+    homeThumbPhoto05.addEventListener("click", () => {
+        photoSave(26);
+    });
+
+    homeThumbPhoto06.addEventListener("click", () => {
+        photoSave(6);
+    });
+
+    homeThumbPhoto07.addEventListener("click", () => {
+        photoSave(33);
+    });
+
+    homeThumbPhoto08.addEventListener("click", () => {
+        photoSave(17);
+    });
+
+    homeThumbPhoto09.addEventListener("click", () => {
+        photoSave(14);
+    });
+
+    homeThumbPhoto10.addEventListener("click", () => {
+        photoSave(20);
+    });
+
+    homeThumbPhoto11.addEventListener("click", () => {
+        photoSave(4);
+    });
+
+    homeThumbPhoto12.addEventListener("click", () => {
+        photoSave(23);
+    });
+
+    homeThumbPhoto13.addEventListener("click", () => {
+        photoSave(22);
+    });
+
+    homeThumbPhoto14.addEventListener("click", () => {
+        photoSave(15);
+    });
+
+    homeThumbPhoto15.addEventListener("click", () => {
+        photoSave(2);
+    });
+
+    homeThumbPhoto16.addEventListener("click", () => {
+        photoSave(24);
+    });
+
+    homeThumbPhoto17.addEventListener("click", () => {
+        photoSave(27);
+    });
+
+    homeThumbPhoto18.addEventListener("click", () => {
+        photoSave(32);
+    });
+
+    homeThumbPhoto19.addEventListener("click", () => {
+        photoSave(34);
+    });
+
+    homeThumbPhoto20.addEventListener("click", () => {
+        photoSave(8);
+    });
+}
+    
+    
+if (document.body.id == "photo-page") {
+
+    nextButton.addEventListener("click", () => {
+        navigatePhotos('next');
+    });
+
+    backButton.addEventListener("click", () => {
+        navigatePhotos('back');
+    });
+
+    body.addEventListener("keydown", (event) => {
+        if (event.isComposing || event.keyCode === 229) {
+        return;
+        }
+    
+        var keyPressed = event.key;
+        navigatePhotos(keyPressed);
+    });
+
+}
+toggleSwitch.addEventListener("click", () => {
+    toggleDarkMode();
+    
+});
+
+function toggleDarkMode() {
+    if (document.documentElement.classList.contains("light")) {
+        document.documentElement.classList.remove("light")
+        document.documentElement.classList.add("dark")
+    } else if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark")
+      document.documentElement.classList.add("light")
+    } 
+    
+  }
+
+
+function photoSave(clickedPhoto) {
+
+    clickedPhotoSave = clickedPhoto;
+    localStorage.setItem("photoNumber", clickedPhotoSave);
+}
+
+function photoView(){
+    var clickedPhotoSave = localStorage.getItem("photoNumber");
+    arrayPhotoPosition = clickedPhotoSave -1;
+    
+    allPhotos[arrayPhotoPosition].style.display = 'grid';
+
+}
+
+
+function navigatePhotos(direction) {
+    if (direction == 'next' || direction == 'ArrowRight') {
+        allPhotos[arrayPhotoPosition].style.display = 'none';
+        arrayPhotoPosition++;
+
+        if(arrayPhotoPosition > 34) {
+            arrayPhotoPosition = 0;
+        }
+
+        allPhotos[arrayPhotoPosition].style.display = 'grid';
+    }
+
+    if (direction == 'back' || direction == 'ArrowLeft') {
+        allPhotos[arrayPhotoPosition].style.display = 'none';
+        arrayPhotoPosition--;
+
+        if(arrayPhotoPosition < 0) {
+            arrayPhotoPosition = 34;
+        }
+
+        allPhotos[arrayPhotoPosition].style.display = 'grid';
+    }
+}
+
+document.querySelector(".burger").addEventListener("click", () =>{
+    const header = document.querySelector("header");
+    if(header.classList.contains("menu-open")) header.classList.remove("menu-open")
+    else header.classList.add("menu-open");
+})
